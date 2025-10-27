5caa81716b9755af2e12fe5ec06aa61ce82770d9
reasonlive <parentaladvicecontrol@gmail.com>
2025-10-27 16:15:25 +0300

    fixed with types

--- app/helpers/DataValidator.js
+++ app/helpers/DataValidator.js
@@ -1,6 +1,6 @@
 export default class DataValidator {
     static validateTemperatureData(data) {
-        return typeof data === 'number'
+        return typeof data === 'number' && (data > 0 && data <= 100)
             || typeof data === 'string' && (parseInt(data) > 0 && parseInt(data) <= 100)
             || (Array.isArray(data) && data.split(',').every(elem => parseInt(elem) >= 0 && parseInt(elem) <= 100))
     }
